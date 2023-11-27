5e3dbee92ae983b41f265920197ba3092f7ce883
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2023-11-27 19:38:55 -0300

    Atualizando lista 6 de exercícios

--- Lista6_Arranjos/Ex3_oMaiorElemento/Ex3_oMaiorElemento.c
+++ Lista6_Arranjos/Ex3_oMaiorElemento/Ex3_oMaiorElemento.c
@@ -1,5 +1,18 @@
 #include "funcaoMasterLista_6.h"
-
+ const colun = 5;
 main(){
+   int vetorTeste[colun];
 
+   for(int i = 0; i < colun; i++){
+
+     printf("Me de o valor que deve ser colocado na posição %d : \n", i  );
+     scanf("%d",&vetorTeste[i]);
+
+   }
+
+    ordenador(vetorTeste, colun);
+
+    for (int i = 0; i < colun; i++){
+        printf(vetorTeste[i]);
+    }
 }
---
+++ Lista6_Arranjos/Ex3_oMaiorElemento/funcaoMasterLista_6.h
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+#include<locale.h>
+
+void inverteVetor(int vetor[],int colunas){
+  int intermediario = 0, i, aux = 1;
+
+      for( i = 0 ; i < colunas /2; i ++){
+        intermediario = vetor[i];
+        vetor[i] = vetor[colunas - aux ];
+        vetor[colunas - aux] = intermediario;
+        aux ++;
+      }
+
+}
+
+void multiplicaVetorPeloIndice(int vetor[],int colunas){
+  int i;
+
+  for( i = 0; i < colunas; i++){
+    vetor[i] = vetor[i]  * (i);
+  }
+}
+
+void multiplicaVetorIndicePonteiro(int *vetor,int coluna){
+
+  int i;
+
+      for(i = 0; i < coluna; i++){
+        vetor[i] = vetor[i] * (i);
+      }
+ }
+
+  void ordenaVetor(int vetor[], int colunas) {
+    int i, aux, menorIndice;
+
+    for (i = 0; i < colunas ){
+
+    }
+ }
+}
