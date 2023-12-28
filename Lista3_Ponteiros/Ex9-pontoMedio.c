8732304189c5e19437a9939fd3d53dfe2155bfce
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2023-12-28 17:04:04 -0300

    Adicionando comentário importante

--- Lista3_Ponteiros/Ex9-pontoMedio.c
+++ Lista3_Ponteiros/Ex9-pontoMedio.c
@@ -10,6 +10,19 @@ void pontoMedio(float x1,float y1,float *x2, float *y2){
 
   *x2 = xM;
   *y2 = yM;
+
+  /*
+       Não, a alocação de memória não poderia ter sido feita dentro do módulo "pontoMedio".
+       Isso porque a função `pontoMedio` recebe ponteiros como parâmetros, e não endereços de variáveis.
+       Se você tentasse alocar memória dentro da função `pontoMedio`,
+       você estaria alterando os valores dos ponteiros locais da função,
+       e não os valores dos ponteiros que foram passados como argumentos.
+       Isso causaria um erro de segmentação, pois você estaria tentando acessar uma área de memória inválida. A
+       lém disso, a memória alocada dentro da função `pontoMedio` seria perdida quando a função terminasse,
+       pois os ponteiros locais sairiam do escopo. Portanto, a alocação de memória deve ser feita fora da função `pontoMedio`,
+       e os ponteiros alocados devem ser passados como argumentos para a função.
+       (fonte: BING by Microsoft)
+   */
 }
 main(){
 
